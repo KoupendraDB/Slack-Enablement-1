a87341139524c9a991f86458d87ad6269e97f696
KoupendraDB <koupenda.dandapani@ferguson.com>
2024-03-13 12:05:00 +0530

    python-mongo

--- python-server/main.py
+++ python-server/main.py
@@ -1,22 +1,23 @@
 from flask import Flask
 from flask_restful import Api
 from resources.user import User
-# from pymongo.mongo_client import MongoClient
-# from pymongo.server_api import ServerApi
-# import os
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+import os
+import certifi
 
-# # Connecting to mongo db
-# mongo_db_username = os.environ['mongo_db_username']
-# mongo_db_password = os.environ['mongo_db_password']
-# uri = "mongodb+srv://{0}:{1}@cluster0.nff3sst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(mongo_db_username, mongo_db_password)
-# # Create a new client and connect to the server
-# client = MongoClient(uri, server_api=ServerApi('1'))
-# # Send a ping to confirm a successful connection
-# try:
-# 	client.admin.command('ping')
-# 	print("Pinged your deployment. You successfully connected to MongoDB!")
-# except Exception as e:
-# 	print(e)
+# Connecting to mongo db
+mongo_db_username = os.environ['mongo_db_username']
+mongo_db_password = os.environ['mongo_db_password']
+uri = "mongodb+srv://{0}:{1}@cluster0.nff3sst.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0".format(mongo_db_username, mongo_db_password)
+# Create a new client and connect to the server
+mongo_client = MongoClient(uri, server_api=ServerApi('1'), tlsCAFile=certifi.where())
+# Send a ping to confirm a successful connection
+try:
+	mongo_client.admin.command('ping')
+	print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+	print(e)
 
 
 app = Flask(__name__)
--- python-server/resources/user.py
+++ python-server/resources/user.py
@@ -1,21 +1,27 @@
 from flask_restful import Resource
 from flask import request
 
-users = []
+def savings(income, expense):
+    try:
+        return income - expense
+    except:
+        return 0
 
 class User(Resource):
     def get(self, user_id):
-        query_result = list(filter(lambda user: user['user_id'] == user_id, users))
-        user = query_result and query_result[0]
+        user = mongo_client.db.users.find_one({'user_id': user_id}, {'_id': 0})
+        print(user)
         if user:
             return {'success': True, 'user': user}, 200
         return {'success': False}, 404
 
     def post(self):
         user_request = request.get_json()
-        query_result = list(filter(lambda user: user['user_id'] == user_request['user_id'], users))
-        user = query_result and query_result[0]
+        user = mongo_client.db.users.find_one({'user_id': user_request['user_id']})
         if user:
-            return {'success': False}, 400
-        users.append(user_request)
+            return {'success': False}, 409
+        user_request['savings'] = savings(user_request['income'], user_request['expense'])
+        mongo_client.db.users.insert_one(user_request)
         return {'success': True}, 201
+
+from main import mongo_client
